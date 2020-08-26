74482893981081fe49321f638dba06358350245c
Paul Thompson <yakulbeans@gmail.com>
2020-08-26 17:09:19 -0600

    Added text for choosing play states -- maybe I should add confirmation sound?

--- states/TitleScreenState.lua
+++ states/TitleScreenState.lua
@@ -15,16 +15,15 @@ function TitleScreenState:update(dt)
 		highlighted = highlighted == 1 and 2 or 1
 		sounds['beep']:play()
 	end
----[[]
-	if highlighted == 1 and love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+	if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+		if highlighted == 1 then
 			sounds['titleMusic']:stop()
 			gStateMachine:change('playState')
-	end
-	if highlighted == 2 and love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+		else
 			sounds['titleMusic']:stop()
 			gStateMachine:change('tripState')
+		end
 	end
---]]
 end
 
 
@@ -33,8 +32,21 @@ function TitleScreenState:render()
 	love.graphics.setFont(normalFont)
 	love.graphics.printf('Trippy Happy', 0, 60, VIRTUAL_WIDTH, 'center')
 	love.graphics.setFont(smallFont)
-	love.graphics.printf('say no drugs', 0, 230, VIRTUAL_WIDTH, 'center')
+	love.graphics.printf('say no  to drugs', 0, 230, VIRTUAL_WIDTH, 'center')
 	love.graphics.setFont(tinyFont)
-	love.graphics.printf('press enter to start', 0 , VIRTUAL_HEIGHT/ 2, VIRTUAL_WIDTH, 'center')
+	love.graphics.printf('would you like drugs?', 0 , VIRTUAL_HEIGHT/ 2 - 10, VIRTUAL_WIDTH, 'center')
+	love.graphics.setFont(smallFont)
+
+	if highlighted == 1 then
+		love.graphics.setColor(80/255, 220/255, 255/255, 255/255)
+	end
+	love.graphics.printf('HELL NAH', 40, VIRTUAL_HEIGHT / 2 + 100, VIRTUAL_WIDTH + 150, 'left')
+	love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+
+	if highlighted == 2 then
+		love.graphics.setColor(80/255, 220/255, 255/255, 255/255)
+	end
+	love.graphics.printf('HELL YAH', 0, VIRTUAL_HEIGHT / 2 + 100, VIRTUAL_WIDTH - 40, 'right')
+	love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
 
 end

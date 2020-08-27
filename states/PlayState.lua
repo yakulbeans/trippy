40d23a692bbc0b92c3bbf9fafd0292724d6eb87c
Paul Thompson <yakulbeans@gmail.com>
2020-08-26 21:19:26 -0600

    added player character only x axis currently for both states

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -4,20 +4,35 @@ function PlayState:init()
 
 end
 
+playerX = 0
+PLAYER_SPEED = 250
 
 function PlayState:update(dt)
 	sounds['playMusic']:setLooping(true)
 	sounds['playMusic']:play()
 
+	if love.keyboard.isDown('right') then
+		playerX = math.min(VIRTUAL_WIDTH - 110, playerX + PLAYER_SPEED * dt)
+	end
+
+	if love.keyboard.isDown('left') then
+		playerX = math.max(0, playerX - PLAYER_SPEED * dt)
+	end
+
 	if love.keyboard.wasPressed('r') then
 		sounds['playMusic']:stop()
 		gStateMachine:change('titleState')
 	end
+
+
 end
 
 
 function PlayState:render()
 	love.graphics.clear(150/255, 150/255, 150/255, 255/255)
 	love.graphics.printf('Hello PlayState', 0, 200, VIRTUAL_HEIGHT / 2, 'center')
+
+	love.graphics.setColor(255/255, 70/255, 70/255, 255/255)
+	love.graphics.rectangle('fill', playerX, VIRTUAL_HEIGHT - 110, 110, 110)
 end 
 
--- states/TripState.lua
+++ states/TripState.lua
@@ -4,10 +4,21 @@ function TripState:init()
 
 end
 
+playerX = 0
+PLAYER_SPEED = 250
 
 function TripState:update(dt)
 	sounds['tripMusic']:setLooping(true)
 	sounds['tripMusic']:play()
+
+	if love.keyboard.isDown('right') then
+		playerX = math.min(VIRTUAL_WIDTH - 110, playerX + PLAYER_SPEED * dt)
+	end
+
+	if love.keyboard.isDown('left') then
+		playerX = math.max(0, playerX - PLAYER_SPEED * dt)
+	end
+
 
 	if love.keyboard.wasPressed('r') then
 		sounds['tripMusic']:stop()
@@ -27,8 +38,14 @@ end
 
 function TripState:render()
 	love.graphics.clear(150/255, 150/255, 150/255, 255/255)
+
+	love.graphics.setColor(255/255, 70/255, 70/255, 255/255)
+	love.graphics.rectangle('fill', playerX, VIRTUAL_HEIGHT - 110, 110, 110)
+
 	love.graphics.draw(redScreen, -redScroll, 0)
 	love.graphics.draw(greenScreen, -greenScroll, 0)
 	love.graphics.draw(blueScreen, -blueScroll, 0)
+
+	love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
 	love.graphics.printf('Hello TripState', 0, 200, VIRTUAL_HEIGHT / 2, 'center')
 end
